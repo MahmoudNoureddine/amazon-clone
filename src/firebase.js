45d5036b7b090c7f40eccebd99827bf818c96085
mahmoud_nourddine <mahmoudnourdein@gmail.com>
2022-04-17 18:13:14 +0200

    header part

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9F8dEmjbSBR8PI9Fea0YuRQUM1Tbwsjc",
+    authDomain: "e-clone-cf064.firebaseapp.com",
+    projectId: "e-clone-cf064",
+    storageBucket: "e-clone-cf064.appspot.com",
+    messagingSenderId: "911623049851",
+    appId: "1:911623049851:web:60c39b76c3a05af9c8c8de"
+  };
